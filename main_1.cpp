501e0e29b7409e6a3b199c776f3d7d80f55f99aa
Maksim Kuzminov <maxfwf@gmail.com>
2023-07-12 20:28:11 +0300

    homework_1

--- main_1.cpp
+++ main_1.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int main()
 {
     char num[100000];
-    while (num[0] != '0')
+    while (!cin.eof())
     {
         cin.getline(num, 10000);
         for (int i = strlen(num); i >= 0; i--)
